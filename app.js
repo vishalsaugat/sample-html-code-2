71f0748f872ddee373248f53e55bc043f286903c
Vishal Saugat <vishalair1@gmail.com>
2021-09-13 23:56:01 +0530

    Added express

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Heyyyyyyyyy--- 8 update')
+const express = require('express')
+const app = express()
+const port = 80 || process.env.PORT
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
